c52c4cf932bdf72734f2f3f3109c0109c7bb62a0
YLb0 <ylber.djemali@abv.bg>
2023-06-19 15:53:36 +0200

    Add Reppo
    Exam

---
+++ C# Advanced Retake Exam - 19 August 2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] n1 = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> zambaket = new Stack<int>(n);
+            Queue<int> trendafilat = new Queue<int>(n1);
+
+            int kurorat = 0;
+            int sum = 0;
+            int kurorat1 = 0;
+            while (zambaket.Count > 0 && trendafilat.Count > 0)
+            {
+                if (zambaket.Peek() + trendafilat.Peek() == 15)
+                {
+                    kurorat++;
+                    zambaket.Pop();
+                    trendafilat.Dequeue();
+                }
+                else if (zambaket.Peek() + trendafilat.Peek() > 15)
+                {
+                    int zamb = zambaket.Pop() - 2;
+                    zambaket.Push(zamb);
+                }
+                else if (zambaket.Peek() + trendafilat.Peek() < 15)
+                {
+                    sum += zambaket.Pop();
+                    sum += trendafilat.Dequeue();
+                }
+            }
+            if (sum >= 15)
+            {
+                kurorat1 = sum / 15;
+            }
+            int total = kurorat + kurorat1;
+            if (total >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {total} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - total} wreaths more!");
+            }
+        }
+    }
+}
---
+++ C# Advanced Retake Exam - 19 August 2020/02.Bee/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace _02._Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            int rowB = 0;
+            int colB = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string input = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        rowB = row;
+                        colB = col;
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+            int count = 0;
+            while (command != "End")
+            {
+                try
+                {
+                    if (command == "up")
+                    {
+                        matrix[rowB, colB] = '.';
+                        rowB--;
+                        if (matrix[rowB, colB] == 'f')
+                        {
+                            matrix[rowB, colB] = '.';
+                            count++;
+                        }
+                        else if (matrix[rowB, colB] == 'O')
+                        {
+                            matrix[rowB, colB] = '.';
+                            rowB--;
+                            if (matrix[rowB, colB] == 'f')
+                            {
+                                matrix[rowB, colB] = '.';
+                                count++;
+                            }
+                        }
+                        matrix[rowB, colB] = 'B';
+                    }
+                    else if (command == "down")
+                    {
+                        matrix[rowB, colB] = '.';
+                        rowB++;
+                        if (matrix[rowB, colB] == 'f')
+                        {
+                            matrix[rowB, colB] = '.';
+                            count++;
+                        }
+                        else if (matrix[rowB, colB] == 'O')
+                        {
+                            matrix[rowB, colB] = '.';
+                            rowB++;
+                            if (matrix[rowB, colB] == 'f')
+                            {
+                                matrix[rowB, colB] = '.';
+                                count++;
+                            }
+                        }
+                        matrix[rowB, colB] = 'B';
+                    }
+                    else if (command == "left")
+                    {
+                        matrix[rowB, colB] = '.';
+                        colB--;
+                        if (matrix[rowB, colB] == 'f')
+                        {
+                            matrix[rowB, colB] = '.';
+                            count++;
+                        }
+                        else if (matrix[rowB, colB] == 'O')
+                        {
+                            matrix[rowB, colB] = '.';
+                            colB--;
+                            if (matrix[rowB, colB] == 'f')
+                            {
+                                matrix[rowB, colB] = '.';
+                                count++;
+                            }
+                        }
+                        matrix[rowB, colB] = 'B';
+                    }
+                    else if (command == "right")
+                    {
+                        matrix[rowB, colB] = '.';
+                        colB++;
+                        if (matrix[rowB, colB] == 'f')
+                        {
+                            matrix[rowB, colB] = '.';
+                            count++;
+                        }
+                        else if (matrix[rowB, colB] == 'O')
+                        {
+                            matrix[rowB, colB] = '.';
+                            colB++;
+                            if (matrix[rowB, colB] == 'f')
+                            {
+                                matrix[rowB, colB] = '.';
+                                count++;
+                            }
+                        }
+                        matrix[rowB, colB] = 'B';
+                    }
+                    command = Console.ReadLine();
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The bee got lost!");
+                    break;
+                }
+            }
+            if (count >= 5)
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {count} flowers!");
+            }
+            else
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - count} flowers more");
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
